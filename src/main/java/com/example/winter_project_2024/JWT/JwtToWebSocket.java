43388c88170fda3c3debc966806d7fce1efccb0f
박규민 <pkm020924@gmail.com>
2024-12-30 18:46:12 +0900

    authorization 쿼리 파라미터로

--- src/main/java/com/example/winter_project_2024/JWT/JwtToWebSocket.java
+++ src/main/java/com/example/winter_project_2024/JWT/JwtToWebSocket.java
@@ -16,12 +16,29 @@ public class JwtToWebSocket implements HandshakeInterceptor {
 
     @Override
     public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler webSocketHandler, Map<String, Object> attributes) throws Exception{
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.isAuthenticated()) {
-            attributes.put("SPRING_SECURITY_CONTEXT", authentication);
-            return true;
+        String queryString = request.getURI().getQuery();
+        if (queryString != null && queryString.contains("token=")) {
+            String token = extractTokenFromQuery(queryString);
+            if(token != null && !token.isEmpty()){
+                Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+                if (authentication != null && authentication.isAuthenticated()) {
+                    attributes.put("SPRING_SECURITY_CONTEXT", authentication);
+                    return true;
+                }
+            }
         }
         return false;
+    }
+
+    private String extractTokenFromQuery(String queryString) {
+        // 쿼리 파라미터에서 "token" 값 추출
+        String[] params = queryString.split("&");
+        for (String param : params) {
+            if (param.startsWith("token=")) {
+                return param.substring("token=".length());
+            }
+        }
+        return null;
     }
 
     @Override
