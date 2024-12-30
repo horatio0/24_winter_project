321f573c3791aba156411eae72ad1933fa698211
박규민 <pkm020924@gmail.com>
2024-12-30 22:57:08 +0900

    websocket 테스트중

--- src/main/java/com/example/winter_project_2024/JWT/JWTFilter.java
+++ src/main/java/com/example/winter_project_2024/JWT/JWTFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -14,6 +15,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Slf4j
 @RequiredArgsConstructor
 public class JWTFilter extends OncePerRequestFilter {
 
@@ -38,6 +40,7 @@ public class JWTFilter extends OncePerRequestFilter {
 
         //Bearer 부분 제거 후 순수 토큰만 추출
         String token = auth.split(" ")[1];
+        log.info(token);
 
         //소멸시간 검증
         if(jwtUtil.isExpired(token)){
